2b3db8f757f918190f50d447538659cb7d94a764
Arseniy Khvorov <khvorov45@gmail.com>
2019-12-18 08:54:12 +1100

    Update sim-test.R

--- tests/sim-test.R
+++ tests/sim-test.R
@@ -14,7 +14,12 @@ test_that("sim_pop works", {
     nvac = get_counts(100, 50, 0.55, nsam, n_days),
     nflu_novac = get_counts(200, 50, 0.12, nsam, n_days),
     ve = 0.5,
-    lag = 0
+    lag = 1
   )
+  pop_wrong <- pop %>%
+    mutate(nflu2 = A_to_E + B0_to_F + C_to_F + B1_to_F) %>%
+    select(nflu2, nflu) %>%
+    filter(nflu2 != nflu)
+  expect_equal(nrow(pop_wrong), 0)
 })
 
